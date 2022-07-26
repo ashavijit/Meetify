23e22cdd470325ed3922d2a566dcadf3c751aa58
ashavijit <as2440@it.jgec.ac.in>
2022-07-26 22:09:24 +0530

    okkkk4

---
+++ src/firebase/config.js
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  // your firebase config
+  apiKey: "AIzaSyAHmFGYkYU8lR5QoVzlAgkkeWzbounf9UQ",
+  authDomain: "meetify1-bd3bd.firebaseapp.com",
+  projectId: "meetify1-bd3bd",
+  storageBucket: "meetify1-bd3bd.appspot.com",
+  messagingSenderId: "21939810359",
+  appId: "1:21939810359:web:0fc35b136c8c73b3e68fb2",
+  measurementId: "G-L6BY6L11HF"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const firestore = getFirestore(app);
+const auth = getAuth(app);
+
+export { auth, firestore };
