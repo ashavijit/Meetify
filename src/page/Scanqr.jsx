4f281e1a7788930bd649813a77b82dd0e4735a03
PRASANT GUPTA <prasant123gupta@gmail.com>
2022-10-12 09:29:58 +0530

    scanner and join via link all set

--- src/page/Scanqr.jsx
+++ src/page/Scanqr.jsx
@@ -1,45 +1,51 @@
-import React, {useState, useRef} from 'react';
+import React, {useState , useEffect} from 'react';
 import { QrReader } from 'react-qr-reader';
+import { useNavigate } from "react-router-dom";
 
 const Scanqr = () => {
 
-  const [text, setText] = useState('');
-  const [imageUrl, setImageUrl] = useState('');
-  const [scanResultFile, setScanResultFile] = useState('');
-  const [scanResultWebCam, setScanResultWebCam] =  useState('helloworld');
-  const qrRef = useRef(null);
+  let navigate = useNavigate();
+  const [roomId, setRoomId] = useState("");
+  const [scanResultWebCam, setScanResultWebCam] =  useState("");
 
-  const onScanFile = () => {
-    qrRef.current.openImageDialog();
-  }
-  const handleErrorWebCam = (error) => {
-    console.log(error);
-  }
-  const handleScanWebCam = (result) => {
-    if (result){
-        setScanResultWebCam(result);
+  useEffect(() => {
+    const indxx = scanResultWebCam.indexOf("/room/");
+    if(indxx == -1) {
+      setRoomId("");
+    } else {
+      setRoomId(scanResultWebCam.slice(indxx));
     }
+  },[scanResultWebCam]);
+
+  const joinroomusingid = () => {
+    navigate(`${roomId}`); 
   }
 
   return (
     <>
-    <div className="joinpage bg-lightGray flex-grow md:h-screen md:border-l-2 border-lightGray p-3 md:p-4">
-      <h3>Qr Code Scan by Web Cam</h3>
+    <div className="joinpage text-white bg-lightGray flex-grow md:h-screen md:border-l-2 border-lightGray p-3 md:p-4">
+      <h3 >Qr Code Scan by Web Cam</h3>
+
+
+      <div className="boxx md:border-1-2 border">
       <QrReader
-        onResult={(result, error) => {
+        onResult={async (result, error) => {
           if (result) {
-            setScanResultWebCam(result?.text);
-            console.log("Meeting Link is:");
-            console.log(result?.text);
+            const ress = result?.text;
+            setScanResultWebCam(ress);
           }
 
           if (error) {
-            console.log(error);
+            // console.log(error);
           }
         }}
-        style={{ width: '100%' }}
+        style={{ width: "100%", height: "100%"}}
       />
-      <h3>Scanned By WebCam Code: {scanResultWebCam}</h3>
+      </div>
+
+      
+      {scanResultWebCam == "" ? <h3>Scan Your Invite QR Align the QR to the camera</h3> : <h3>Meeting Invite Link is:<br /> {scanResultWebCam}</h3>}
+      {roomId == "" ? <button className="my-11 px-4 bg-red py-1 rounded-lg border border-danger" disabled>Scan the QR</button> : <button className="my-11 px-4 py-1 bg-blue rounded-lg border border-danger" onClick={joinroomusingid}>Join Meeting</button>}
     </div>
     </>
   )
