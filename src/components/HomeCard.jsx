be841bfbc3d87202f00b87fa9ec917bc631c6137
Maxim N <mnosov622@gmail.com>
2022-11-02 10:12:18 +0700

    screen share functionality added

--- src/components/HomeCard.jsx
+++ src/components/HomeCard.jsx
@@ -1,15 +1,48 @@
 import React from "react";
-
 const HomeCard = ({ icon, title, desc, bgColor, route }) => {
+  function dumpOptionsInfo() {
+    const videoTrack = videoElem.srcObject.getVideoTracks()[0];
+    console.log(videoTrack);
+
+    console.info("Track settings:");
+    console.info(JSON.stringify(videoTrack.getSettings(), null, 2));
+    console.info("Track constraints:");
+    console.info(JSON.stringify(videoTrack.getConstraints(), null, 2));
+  }
+
+  const videoElem = document.getElementById("video");
+  let displayMediaOptions = {
+    video: {
+      cursor: "always",
+      height: 1000,
+      width: 1200,
+    },
+    audio: false,
+  };
+
+  async function startCapture() {
+    try {
+      videoElem.srcObject = await navigator.mediaDevices.getDisplayMedia(displayMediaOptions);
+      dumpOptionsInfo();
+    } catch (err) {
+      console.error("Error: " + err);
+    }
+  }
+
   return (
     <div
-      className={` p-3 md:p-6 rounded md:rounded-2xl md:h-52 md:w-auto w-full md:aspect-square group ${bgColor}`}
+      onClick={startCapture}
+      className={` p-3 md:p-6 rounded md:rounded-2xl md:h-52 md:w-auto w-full md:aspect-square group ${bgColor} card`}
     >
-      <div className="flex md:h-full items-center md:flex-col md:items-start md:justify-between gap-2">
+      <div className="hidden">
+        <video id="video" autoPlay muted></video>
+      </div>
+
+      <div className="flex md:h-full items-center md:flex-col md:items-start md:justify-between gap-2 cursor-pointer">
         <div
           className={`text-white text-2xl aspect-square md:h-12 flex items-center justify-center md:text-3xl 
           md:bg-slate-400/40 group-hover:text- group-hover:scale-110 duration-200 md:p-2 md:border-[1px]
-           md:border-slate-300/40  rounded-lg`}
+           md:border-slate-300/40  rounded-lg `}
         >
           {icon}
         </div>
